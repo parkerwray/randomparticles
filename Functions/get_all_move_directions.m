7efe602a783da6d9c99281e2c8b93e5cc9233c0a
eg-paul <eligpaul@gmail.com>
2020-06-19 15:06:46 -0400

    Fixed center particle and patched buggy fill fraction display

--- Functions/get_all_move_directions.m
+++ Functions/get_all_move_directions.m
@@ -5,21 +5,23 @@ function delta = get_all_move_directions(cords, radii, lower_bounds, upper_bound
 
     delta = zeros(length(cords),3);
     for idx1 = 1:size(cords, 1)
-       dummy = cords;
-       dummy(idx1,:) = [];
-       dummyr = radii;
-       dummyr(idx1) = [];
-       
-       
-        new_cord = make_mirror_3D(cords(idx1,:), radii(idx1), ...
-            lower_bounds, upper_bounds);
-        
-       for idx2 = 1:size(new_cord, 1)
-          pushes = check_touch_pushes(new_cord(idx2,:), dummy, ...
-              radii(idx1), dummyr, lower_bounds, upper_bounds);
-          for idx = 1:size(pushes, 1)
-             delta(idx1,:) = delta(idx1,:) + pushes(idx,:); 
-          end
+       if ~isequal(cords(idx1,:), [0,0,0])
+           dummy = cords;
+           dummy(idx1,:) = [];
+           dummyr = radii;
+           dummyr(idx1) = [];
+
+
+            new_cord = make_mirror_3D(cords(idx1,:), radii(idx1), ...
+                lower_bounds, upper_bounds);
+
+           for idx2 = 1:size(new_cord, 1)
+              pushes = check_touch_pushes(new_cord(idx2,:), dummy, ...
+                  radii(idx1), dummyr, lower_bounds, upper_bounds);
+              for idx = 1:size(pushes, 1)
+                 delta(idx1,:) = delta(idx1,:) + pushes(idx,:); 
+              end
+           end
        end
        %if norm(delta(idx1,:)) ~= 0
        %   delta(idx1,:) = delta(idx1,:) ./ norm(delta(idx1,:)); 
--- Functions/random_move.m
+++ Functions/random_move.m
@@ -1,6 +1,9 @@
 function new_cord = random_move(cord, r, dimension)
 % Randomly move the particle +1, 0, or -1 in the x, y, and z dimensions.
 r = floor(r);
+if r == 0
+    r = 1;
+end
 if dimension == 2
        new_cord = [cord(1)+randi(r).*round(2*rand-1),...
        cord(2)+randi(r).*round(2*rand-1),...
